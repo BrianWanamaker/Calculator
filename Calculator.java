1e1a56f05395312120fb223b8f0bb41c25e39e6f
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-02-07 19:10:59 -0500

    Addition added

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,15 @@
-public class Calculator{
-    
-    public static void mai(String[]args)
-    {
+import java.util.Scanner;
 
+public class Calculator{
+
+    public static void main(String[]args)
+    {
+        System.out.println("Enter two whole numbers");
+        System.out.println("Number 1:");
+        Scanner in = new Scanner(System.in);
+        int numOne = in.nextInt();
+        System.out.println("Number 2:");
+        int numTwo = in.nextInt();
+        System.out.println(numOne + "+" + numTwo + " = " + (numOne+numTwo));
     }
 }
